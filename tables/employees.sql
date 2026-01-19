b125a1fc1f10237dd3d50b1b5f810f23ca74676b
Anshuman <ansh2003man@gmail.com>
2026-01-20 02:51:39 +0530

    tables provided

---
+++ tables/employees.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE employees (
+    employee_id     NUMBER AUTOINCREMENT PRIMARY KEY,
+    first_name      VARCHAR(50) NOT NULL,
+    last_name       VARCHAR(50) ,
+    email           VARCHAR(100) UNIQUE,
+    phone_number    VARCHAR(15) UNIQUE,
+    salary          NUMBER(10,2) CHECK (salary > 0),
+    department_id   NUMBER NOT NULL,
+    hire_date       DATE DEFAULT CURRENT_DATE(),
+    CONSTRAINT fk_dept FOREIGN KEY (department_id)
+        REFERENCES departments(department_id)
+);
+
